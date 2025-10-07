8cd244bf388977667f22ed849076d116aed752ba
dotfelixb@gmail.com <dotfelixb@gmail.com>
2025-10-07 13:45:24 +0000

    place tiles on board

--- src/Fifteen.fs
+++ src/Fifteen.fs
@@ -5,6 +5,19 @@ open Feliz
 
 let rnd = Random()
 
+let computeRow index =
+    int(Math.Floor(float(index - 1) / 4.0)), (index - 1) % 4
+
+let computeBounds row col =
+    [
+        row - 1, col
+        row + 1, col
+        row, col - 1
+        row, col + 1
+    ]  |> List.filter (fun (r,c) -> r >= 0 && r < 4 && c >= 0 && c < 4)
+
+let freeSlot = rnd.Next(1, 16)
+
 [<ReactComponent>]
 let Tile number clickable =
 
@@ -17,14 +30,15 @@ let Tile number clickable =
         prop.text (string number)
     ]
 
-let computeRow index =
-    int(Math.Floor(float(index - 1) / 4.0)), (index - 1) % 4
-
-let freeSlot = rnd.Next(1, 16)
+let generateTiles = 
+    [1 .. 15]
+    |> List.map (fun i -> 
+        Tile (string i) true
+    )    
 
 [<ReactComponent(true)>]
 let Game () = 
-    
+    let mutable index = 0
     Html.div [
         prop.className "flex h-screen w-full items-center justify-center"
         prop.children [
@@ -35,13 +49,15 @@ let Game () =
                         prop.className "grid grid-cols-4 gap-2"
                         prop.children [
                             for i in 1 .. 16 do
+                                let row, col = computeRow i
+                                let bounds = computeBounds row col
                                 Html.div [
                                     prop.className "bg-teal-800 flex rounded-xl h-24 w-24 box-border"
-                                    // prop.text (computeRow i |> sprintf "%A" )
-                                    prop.children [
-                                        if i <> freeSlot then
-                                            Tile (computeRow i) true
-                                    ]
+                                    prop.onClick (fun _ -> printfn "Clicked %A" bounds)
+                                    // prop.text (sprintf "%i = (%i, %i)" i row col)
+                                    if i <> freeSlot then
+                                        generateTiles.[index] |> prop.children
+                                        index <- index + 1
                                 ] 
                         ]
                     ]
